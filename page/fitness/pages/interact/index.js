584504fcac3c4291acdf77c9d203afaca725bfb9
Ann <601248452@qq.com>
2021-03-08 09:18:26 +0800

    提交

--- page/fitness/pages/interact/index.js
+++ page/fitness/pages/interact/index.js
@@ -15,7 +15,7 @@ Page({
     // 组件所需的参数
     nvabarData: {
      showCapsule: 1, //是否显示左上角图标   1表示显示    0表示不显示
-     title: '健康互动', //导航栏 中间的标题,
+     title: '互动视频', //导航栏 中间的标题,
      isBackPer: true, //不显示返回按钮,
      bgColor:'#f4424a' //导航背景色
    },
